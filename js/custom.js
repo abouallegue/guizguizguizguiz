820cfdfd1c1dac1ca8b99a1350beacc3b9c4b4bb
AnisBou <bouallegue70@gmail.com>
2022-01-17 22:03:46 +0100

    add

--- js/custom.js
+++ js/custom.js
@@ -18,8 +18,8 @@
     // ABOUT SLIDER
     $('body').vegas({
         slides: [
-            { src: 'images/slide-image01.jpg' },
-            { src: 'images/slide-image02.jpg' }
+            { src: 'images/plots.jpg' },
+            { src: 'images/grands_plots.jpg' }
         ],
         timer: false,
         transition: [ 'zoomOut', ]
